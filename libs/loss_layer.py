64e6b0ebfae94aeae2b29afd44ac8fd3f6e9b213
こしあん <saltcoffeehorse@gmail.com>
2019-04-01 22:17:52 +0900

    Update loss_layer.py

--- libs/loss_layer.py
+++ libs/loss_layer.py
@@ -27,7 +27,8 @@ class LossLayer(Layer):
         tv = total_variation_loss(y_true, y_pred)
 
         # 全体の損失関数
-        total_loss = content + style + tv
+        #total_loss = content + style + tv
+        total_loss = content
 
         # (batch,H,W,1)のテンソルを作る
         ones = K.sign(K.abs(y_pred) + 1) # (batch,H,W,3)のすべて1のテンソル
