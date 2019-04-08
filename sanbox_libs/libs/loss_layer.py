0c8198d412b447609650b3dd093b00ecaf74671d
こしあん <saltcoffeehorse@gmail.com>
2019-04-09 06:50:50 +0900

    Update loss_layer.py

--- sanbox_libs/libs/loss_layer.py
+++ sanbox_libs/libs/loss_layer.py
@@ -32,7 +32,7 @@ class LossLayer(Layer):
 
         # 全体の損失関数
         #total_loss = l1 + 6*l2 + 0.05*l3 + 120*(l4+l5) + 0.1*l6
-        total_loss = l1 + 6*l2 + 0.05*l3 + 1*(l4+l5) + 0.1*l6
+        total_loss = 10*l1 + 60*l2 + 0.05*l3 + 1*(l4+l5) + 1*l6
         #total_loss = l1 + 6*l2
 
         # (batch,H,W,1)のテンソルを作る
