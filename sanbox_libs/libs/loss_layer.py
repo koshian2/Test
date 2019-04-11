15d0a020fd07ee2fb80031da86c6641992f24040
こしあん <saltcoffeehorse@gmail.com>
2019-04-12 00:12:22 +0900

    Update loss_layer.py

--- sanbox_libs/libs/loss_layer.py
+++ sanbox_libs/libs/loss_layer.py
@@ -35,12 +35,8 @@ class LossLayer(Layer):
 
         # 全体の損失関数
         #total_loss = l1 + 6*l2 + 0.05*l3 + 120*(l4+l5) + 0.1*l6
-        #total_loss = 10*l1 + 60*l2 + 0.05*l3 + 1*(l4+l5) + 1*l6
-        #total_loss = 10*l1 + 60*l2 + 0.05*l3 + 10*(l4+l5) + 1*l6
-        #total_loss = 1*l1 + 60*l2 + 0.05*l3 + 1*(l4+l5) + 1*l6
-        #total_loss = 1*l1 + 60*l2 + 0.05*l3 + 1*(l4+l5) + 1*l6 + 20*l7
-        total_loss = 10*l1 + 60*l2 + 0.005*l3 + 0.5*(l4+l5) + 0.1*l6
-        #total_loss = 10*l1 + 60*l2 + 0.05*l3 + 1*(l4+l5) + 0.1*l6 + 60*l7
+        total_loss = 10*l1 + 60*l2 + 0.05*l3 + 1*(l4+l5) + 1*l6
+        #total_loss = 10*l1 + 60*l2 + 0.005*l3 + 0.5*(l4+l5) + 0.1*l6
         
         # (batch,H,W,1)のテンソルを作る
         ones = K.sign(K.abs(y_pred) + 1) # (batch,H,W,3)のすべて1のテンソル
